1a5f8c28d90eab672c158f89cd97714d1958d53e
Benjamin Rio <benjamin.rio@student-cs.fr>
2022-05-14 11:39:31 +0200

    counts on popup load

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
-document.getElementById("btn").addEventListener("click", async() => {
+window.addEventListener("load", () => {
+    countTabs();
+});
+
+const countTabs = async() => {
     let tabs = await chrome.tabs.query((queryInfo = {}));
     let counter = (document.getElementById(
         "counter"
     ).innerHTML = `counter: ${tabs.length}`);
-});
+};
