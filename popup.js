f8fa3d8ce9cbbcb2a4ed261466158c87fe62de3b
Benjamin Rio <benjamin.rio@student-cs.fr>
2022-05-15 18:45:40 +0200

    adaptive color

--- popup.js
+++ popup.js
@@ -4,5 +4,12 @@ window.addEventListener("load", () => {
 
 const countTabs = async() => {
     let tabs = await chrome.tabs.query((queryInfo = {}));
-    document.getElementById("counter").innerHTML = `counter: ${tabs.length}`;
+    let counter = document.getElementById("counter");
+    counter.innerHTML = `counter: ${tabs.length}`;
+    counter.style.color = computeContinuousColor(tabs.length);
 };
+
+function computeContinuousColor(counter) {
+    let red = Math.min(parseInt(counter) * 10, 255);
+    return `rgb(${red}, 0 ,0)`;
+}
